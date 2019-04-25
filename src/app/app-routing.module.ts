7f2a3d4adb42949497a96a4e04d3bdb0ffdd245a
willy <willkoua@gmail.com>
2019-04-25 00:59:38 -0400

    refactoring architecture

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,41 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import {LoginLayoutComponent} from './layouts/login-layout/login-layout.component';
-import {LoginComponent} from './components/pages/login/login.component';
-import {SignupComponent} from './components/pages/signup/signup.component';
-import {RegisterActivationComponent} from './components/pages/register-activation/register-activation.component';
-import {ForgotPasswordComponent} from './components/pages/forgot-password/forgot-password.component';
-import {ResetPasswordComponent} from './components/pages/reset-password/reset-password.component';
-
-const appRoutes: Routes = [{
-  path: 'auth',
-  component: LoginLayoutComponent,
-  children: [
-    { path: '', component: LoginComponent },
-    { path: 'signup', component: SignupComponent },
-    {
-      path: 'register/activate/:token',
-      component: RegisterActivationComponent
-    },
-    {
-      path: 'forgot-password',
-      component: ForgotPasswordComponent,
-    },
-    {
-      path: 'reset-password/:token',
-      component: ResetPasswordComponent,
-    },
-  ]
-}];
+const appRoutes: Routes = [
+  { path: 'auth', loadChildren: './components/user/user.module#UserModule' },
+];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(
-      appRoutes,
-      {
-        enableTracing: true,
-      })
+    RouterModule.forRoot(appRoutes)
   ],
   exports: [RouterModule]
 })
---
+++ src/app/components/user/user.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { SharedModule} from '../shared/shared.module';
+import { UserRoutingModule } from './user-routing.module';
+import { ForgotPasswordComponent} from './forgot-password/forgot-password.component';
+import { LoginComponent } from './login/login.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { RegisterActivationComponent} from './register-activation/register-activation.component';
+import { SignupComponent} from './signup/signup.component';
+import { LoginLayoutComponent} from '../../layouts/login-layout/login-layout.component';
+
+@NgModule({
+  declarations: [
+    ForgotPasswordComponent,
+    SignupComponent,
+    ResetPasswordComponent,
+    RegisterActivationComponent,
+    LoginComponent,
+    LoginLayoutComponent
+  ],
+  imports: [
+    CommonModule,
+    UserRoutingModule,
+    SharedModule,
+    NgbCarouselModule,
+  ]
+})
+export class UserModule { }
