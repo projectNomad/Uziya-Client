43021bbd799ea572aba9b441f45751d1833ff95f
willy <willkoua@gmail.com>
2019-04-28 09:59:46 -0400

    add upload module

--- src/app/components/admin/admin-routing.module.ts
+++ src/app/components/admin/admin-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import {AdminLayoutComponent} from '../../layouts/admin-layout/admin-layout.component';
 import {HomeComponent} from './home/home.component';
 import {MoviesComponent} from './movies/movies.component';
+import {RegisterVideoComponent} from './movies/register-video/register-video.component';
 
 const routes: Routes = [
   {
@@ -12,6 +13,7 @@ const routes: Routes = [
     children: [
       { path: '', component: HomeComponent },
       { path: 'movies', component: MoviesComponent },
+      { path: 'movies/upload', component: RegisterVideoComponent },
     ]
   }
 ];
--- src/app/components/admin/admin.module.ts
+++ src/app/components/admin/admin.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { NgxUploaderModule } from 'ngx-uploader';
+import { NgSelectModule } from '@ng-select/ng-select';
+import {NgbDropdownModule} from '@ng-bootstrap/ng-bootstrap';
+
 import { AdminRoutingModule } from './admin-routing.module';
 import { SharedModule} from '../shared/shared.module';
 import { AdminLayoutComponent } from '../../layouts/admin-layout/admin-layout.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderAdminComponent} from './shared/header-admin/header-admin.component';
 import { MenuLeftComponent } from './shared/menu-left/menu-left.component';
-import {NgbDropdownModule} from '@ng-bootstrap/ng-bootstrap';
 import { MoviesComponent } from './movies/movies.component';
+import { RegisterVideoComponent } from './movies/register-video/register-video.component';
+import { HeaderPageComponent } from './shared/header-page/header-page.component';
 
 
 @NgModule({
@@ -17,13 +22,17 @@ import { MoviesComponent } from './movies/movies.component';
     HomeComponent,
     HeaderAdminComponent,
     MenuLeftComponent,
-    MoviesComponent
+    MoviesComponent,
+    RegisterVideoComponent,
+    HeaderPageComponent
   ],
   imports: [
     CommonModule,
     AdminRoutingModule,
     SharedModule,
     NgbDropdownModule,
+    NgxUploaderModule,
+    NgSelectModule
   ]
 })
 export class AdminModule { }
