c8f473fd6427b67ac170bcc7836e7ffd1835bb1a
willy <willkoua@gmail.com>
2019-04-29 23:04:59 -0400

    add details video component

--- src/app/components/admin/home/home.component.ts
+++ src/app/components/admin/home/home.component.ts
@@ -1,6 +1,5 @@
-import {Component, OnInit, render } from '@angular/core';
-import { Subject } from 'rxjs';
-import { map } from 'rxjs/operators';
+import {Component, OnInit } from '@angular/core';
+
 
 import {NgbDropdownConfig} from '@ng-bootstrap/ng-bootstrap';
 
@@ -14,69 +13,9 @@ import {MoviesService} from '../../../services/movies.service';
 })
 export class HomeComponent implements OnInit {
   breadcrumb: any[];
-  dtOptions: DataTables.Settings = {};
-  // We use this trigger because fetching the list of persons can be quite long,
-  // thus we ensure the data is fetched before rendering
-  dtTrigger: any;
-  videos: any;
 
-  constructor(
-    private movieService: MoviesService
-  ) {
-    this.dtTrigger = new Subject();
-    this.breadcrumb = [
-      {
-        title: 'home',
-        link: null
-      }
-    ];
-  }
+  constructor() {}
 
   ngOnInit() {
-    this.dtOptions = {
-      pagingType: 'simple_numbers',
-      pageLength: 6,
-      columns: [
-        { title: 'id', visible: false },
-        { title: 'Title'},
-        {
-          title: 'Is active',
-          searchable: false,
-          className: 'text-center'
-        },
-        {
-          data: 'duration',
-          render: function (data: any, type: any, full: any) {
-            return (data / 60).toFixed(2);
-          }
-        },
-        {
-          title: 'genres'
-        }
-      ],
-      rowCallback: (row: Node, data: any[] | Object, index: number) => {
-        const self = this;
-        // Unbind first in order to avoid any duplicate handler
-        // (see https://github.com/l-lin/angular-datatables/issues/87)
-        $('td', row).unbind('click');
-        $('td', row).bind('click', () => {
-          self.someClickHandler(data);
-        });
-        return row;
-      }
-    };
-
-    this.movieService.getListVideos()
-      .pipe(map(result => result.results))
-      .subscribe(
-      value => {
-        this.videos = value;
-        this.dtTrigger.next();
-      }
-    );
-  }
-
-  someClickHandler(data) {
-    console.log(data);
   }
 }
--- src/app/components/admin/movies/movies.component.ts
+++ src/app/components/admin/movies/movies.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import {Router} from '@angular/router';
+
+import {MoviesService} from '../../../services/movies.service';
 
 @Component({
   selector: 'app-movies',
@@ -6,29 +11,80 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movies.component.scss']
 })
 export class MoviesComponent implements OnInit {
-  buttonsEnd: any[];
-  breadcrumb: any[];
+  breadcrumb = [
+    {
+      title: 'home',
+      link: '/admin'
+    },
+    {
+      title: 'movies',
+      link: null
+    },
+  ];
+  buttonsEnd = [
+    {
+      title: 'upload video',
+      link: '/admin/movies/upload'
+    },
+  ];
+  dtOptions: DataTables.Settings = {};
+  // We use this trigger because fetching the list of persons can be quite long,
+  // thus we ensure the data is fetched before rendering
+  dtTrigger: any;
+  videos: any;
 
-  constructor() {
-    this.breadcrumb = [
-      {
-        title: 'home',
-        link: '/admin'
-      },
-      {
-        title: 'movies',
-        link: null
-      },
-    ];
-    this.buttonsEnd = [
-      {
-        title: 'upload video',
-        link: '/admin/movies/upload'
-      },
-    ];
+  constructor(
+    private movieService: MoviesService,
+    private router: Router,
+  ) {
+    this.dtTrigger = new Subject();
   }
 
   ngOnInit() {
+    this.dtOptions = {
+      pagingType: 'simple_numbers',
+      pageLength: 6,
+      columns: [
+        { title: 'id', visible: false },
+        { },
+        {
+          searchable: false,
+          className: 'text-center',
+          width: '70px'
+        },
+        {
+          width: '70px',
+          render: function (data: any, type: any, full: any) {
+            return (data / 60).toFixed(2);
+          }
+        },
+        {}
+      ],
+      rowCallback: (row: Node, data: any[] | Object, index: number) => {
+        const self = this;
+        // Unbind first in order to avoid any duplicate handler
+        // (see https://github.com/l-lin/angular-datatables/issues/87)
+        $('td', row).unbind('click');
+        $('td', row).bind('click', () => {
+          self.someClickHandler(data);
+        });
+        return row;
+      }
+    };
+
+    this.movieService.getListVideos()
+      .pipe(map(result => result.results))
+      .subscribe(
+        value => {
+          this.videos = value;
+          this.dtTrigger.next();
+        }
+      );
+  }
+
+  someClickHandler(data) {
+    console.log(data[0]);
+    this.router.navigate(['/admin/movies/details/', data[0]]);
   }
 
 }
